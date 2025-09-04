c2927b1b798fe4a6935f20a3bd1ceebe16d15016
SuperMechaDeathChrist <miguel_nunez95@hotmail.com>
2025-09-04 03:21:43 -0600

    added _site
    added _site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1756976206',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/documentation/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
